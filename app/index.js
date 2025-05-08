5ee1d8ca8f49987d535a36f3b3ecb0cafc3b460a
nafisah <abdulkadirnafisahabidemi@gmail.com>
2025-05-08 16:52:42 +0200

    Initial commit: Node.js app with k8s manifests

---
+++ app/index.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("Hello from Node.js on Kubernetes via Argo CD!");
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
